a4797051da0732e7d292196f4ebbc718edccce9f
Yichen Wei <weiyichen1999@gmail.com>
2017-08-21 23:09:26 +0800

    modified:   events/views.py

--- events/views.py
+++ events/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Event
 from .forms import eventChangeForm, eventCreateForm
-import datetime
+import datetime, base64, hashlib
 
 
 def index(request):
-    event_list = Event.objects.order_by('-time')
+    event_list = Event.objects.filter(time__gte=datetime.date.today()).order_by('time')
     return render(request, 'events/index.html', {'event_list': event_list})
 
 
@@ -15,6 +15,10 @@ def detailed(request, event_hash):
 
 
 def edit(request, event_hash):
+    # if user does not have permission, refuse access.
+    if not request.user.has_perm('events.can_add_event'):
+        return render(request, 'permission_error.html')
+
     event = get_object_or_404(Event, hash=event_hash)
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
@@ -25,7 +29,7 @@ def edit(request, event_hash):
             # process the data in form.cleaned_data as required
             # ...
             # redirect to a new URL:
-            event.time = form1.cleaned_data['time']
+            event.date = form1.cleaned_data['time']
             event.save()
             return redirect("../")
 
@@ -36,6 +40,10 @@ def edit(request, event_hash):
 
 
 def add_new(request):
+    # if user does not have permission, refuse access.
+    if not request.user.has_perm('events.can_add_event'):
+        return render(request, 'permission_error.html')
+
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
         form = eventCreateForm(request.POST)
@@ -44,14 +52,21 @@ def add_new(request):
             # process the data in form.cleaned_data as required
             # ...
             # redirect to a new URL:
-            Event.objects.create(name=form.cleaned_data['name'],
-                                 time=form.cleaned_data['time'],
+            event_name = form.cleaned_data['name']
+            event_time = form.cleaned_data['time']
+            event_type = form.cleaned_data['type']
+            event_hash = base64.b64encode(hashlib.sha1(
+                (event_name + event_time.strftime('%m-%d-%Y') + event_type).encode('utf-8')).digest()).decode('utf-8')[
+                         0:11].replace("/", "_")
+            Event.objects.create(name=event_name,
+                                 time=event_time,
                                  description=form.cleaned_data['description'],
-                                 type=form.cleaned_data['type'],
+                                 type=event_type,
                                  publish_date=datetime.datetime.now(),
-                                 is_private=0
+                                 is_private=0,
+                                 hash=event_hash
                                  )
-            return redirect("/")
+            return redirect("/events/" + event_hash + "/")
         else:
             return render(request, 'events/new.html', {'form': form})
     else:
